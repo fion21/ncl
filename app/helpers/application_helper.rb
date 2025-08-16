bd2e088dcebd06566e334284ca3a0e070d9adbb9
Fiona L <hwindepth@yahoo.com>
2025-08-16 21:58:33 +0100

    Installed styles for applicaiton layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,55 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(style = '')
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{link_to 'contact me', contact_path} if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
+  # Remove external dependency; return the copyright HTML directly
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    "&copy; #{Time.current.year} Fiona Lewis — All rights reserved".html_safe
+  end
+
+  def nav_items
+    [
+      { url: root_path,       title: 'Home' },
+      { url: about_me_path,   title: 'About Me' },
+      { url: contact_path,    title: 'Contact' },
+      { url: blogs_path,      title: 'Blog' },
+      { url: portfolios_path, title: 'Portfolio' },
+      { url: tech_news_path,  title: 'Tech News' }
+    ]
+  end
+
+  def nav_helper(style, tag_type)
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    "active" if current_page?(path)
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    alert_generator(alert) if alert
+  end
+
+  def alert_generator(msg)
+    js add_gritter(msg, title: "Fiona Lewis Portfolio", sticky: false)
   end
 end
